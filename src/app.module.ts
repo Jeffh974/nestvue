1d946cc794922dc08703048aa3b78e8b4595f219
DevMAC <jeff.hoarau@gmail.com>
2022-04-28 23:19:42 +0200

    Initial commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from '@nestjs/common'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { ServeStaticModule } from '@nestjs/serve-static' // New
+import { join } from 'path' // New
 
 @Module({
-  imports: [],
+  imports: [
+    ServeStaticModule.forRoot({
+      // New
+      rootPath: join(__dirname, '..', 'client/dist') // New
+    }) // New
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService]
 })
 export class AppModule {}
